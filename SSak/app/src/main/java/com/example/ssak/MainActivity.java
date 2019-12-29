19a22fae76fb564186674d5d565db48344c9a119
soo0limm <stellar1198@gmail.com>
2019-12-29 23:32:26 +0900

    set a button on MainActivity

--- SSak/app/src/main/java/com/example/ssak/MainActivity.java
+++ SSak/app/src/main/java/com/example/ssak/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.ssak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+// Customized by SY
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        uploadProducts();
+    }
+
+    public void uploadProducts() {
+        ImageView button = findViewById(R.id.product_upload);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), UploadProductActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private long time = 0;
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+
+        if (System.currentTimeMillis() >= 2000 + time) {
+            time = System.currentTimeMillis();
+            Toast.makeText(getApplicationContext(), "뒤로 가기 버튼을 한 번 더 누르면 종료합니다.", Toast.LENGTH_SHORT).show();
+        } else if (System.currentTimeMillis() < 2000 + time) {
+            finish();
+        }
     }
 }
