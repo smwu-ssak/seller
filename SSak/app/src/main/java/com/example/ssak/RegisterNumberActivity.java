6542c76ec58124278e12c499f3b5b617b591752f
soo0limm <stellar1198@gmail.com>
2019-12-31 16:56:32 +0900

    add page moving buttons

--- SSak/app/src/main/java/com/example/ssak/RegisterNumberActivity.java
+++ SSak/app/src/main/java/com/example/ssak/RegisterNumberActivity.java
@@ -2,7 +2,10 @@ package com.example.ssak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class RegisterNumberActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class RegisterNumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_number);
+
+        goToPreviousPage();
+        goToNextPage();
+    }
+
+    public void goToPreviousPage() {
+        LinearLayout btn = findViewById(R.id.register_number_act_back_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterTimeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    public void goToNextPage() {
+        LinearLayout btn = findViewById(R.id.register_number_act_next_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
