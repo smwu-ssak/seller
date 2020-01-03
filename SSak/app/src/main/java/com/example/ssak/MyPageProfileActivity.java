cd073b7ab657108dbd40e7e8b8e32275abe972a2
chantmsss <49mgntic@gmail.com>
2020-01-03 21:10:15 +0900

    Connect Server with Client

--- SSak/app/src/main/java/com/example/ssak/MyPageProfileActivity.java
+++ SSak/app/src/main/java/com/example/ssak/MyPageProfileActivity.java
@@ -3,12 +3,59 @@ package com.example.ssak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.ssak.DB.SharedPreferenceController;
+import com.example.ssak.Get.GetKakaoProfileResponse;
+import com.example.ssak.Network.ApplicationController;
+import com.example.ssak.Network.NetworkService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MyPageProfileActivity extends AppCompatActivity {
+
+    ApplicationController applicationController = new ApplicationController();
+    NetworkService networkService = applicationController.buildNetworkService();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page_profile);
+
+        requestKakaoProfileDataToServer();
+
     }
+
+    public void requestKakaoProfileDataToServer() {
+        Call<GetKakaoProfileResponse> call = networkService.getKakaoProfileResponse("application/json", SharedPreferenceController.getMyId(getApplicationContext()));
+        call.enqueue(new Callback<GetKakaoProfileResponse>() {
+            @Override
+            public void onResponse(Call<GetKakaoProfileResponse> call, Response<GetKakaoProfileResponse> response) {
+                if (response.isSuccessful()) {
+                    String imgUrl = response.body().data.userProfile;
+                    String name = response.body().data.userName;
+
+                    ImageView imageView = findViewById(R.id.mypage_profile_act_user_img);
+                    Glide.with(getApplicationContext())
+                            .load(imgUrl)
+                            .into(imageView);
+                    TextView textView = findViewById(R.id.mypage_profile_act_store_name);
+                    textView.setText(name);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<GetKakaoProfileResponse> call, Throwable t) {
+
+            }
+        });
+
+
+    }
+
+
 }
