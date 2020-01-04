a42002e8ec8c1ab10d8cd8e5079c553a9d6d0dd4
soo0limm <stellar1198@gmail.com>
2020-01-04 10:43:59 +0900

    prepare NetworkService for getting latitude, longitude

---
+++ SSak/app/src/main/java/com/example/ssak/Get/GetStoreAddressResponse.java
@@ -0,0 +1,15 @@
+package com.example.ssak.Get;
+
+import com.example.ssak.data.StoreAddressDocumentData;
+import com.example.ssak.data.StoreAddressMetaData;
+
+import java.util.ArrayList;
+
+// Customized by SY
+
+public class GetStoreAddressResponse {
+
+    public ArrayList<StoreAddressDocumentData> documents;
+    public StoreAddressMetaData meta;
+
+}
---
+++ SSak/app/src/main/java/com/example/ssak/data/StoreAddressDocumentData.java
@@ -0,0 +1,10 @@
+package com.example.ssak.data;
+
+// Customized by SY
+
+public class StoreAddressDocumentData {
+
+    public String x;    // log
+    public String y;    // lat
+
+}
