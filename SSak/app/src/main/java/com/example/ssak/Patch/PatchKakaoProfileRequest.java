44d398c5e1098702c803fd40fbaaf3e0e7ec5c39
soo0limm <stellar1198@gmail.com>
2020-01-04 17:23:04 +0900

    connect Server with Client - upload Kakao Profile

---
+++ SSak/app/src/main/java/com/example/ssak/Patch/PatchKakaoProfileRequest.java
@@ -0,0 +1,11 @@
+package com.example.ssak.Patch;
+
+// Customized by SY
+
+public class PatchKakaoProfileRequest {
+
+    public int status;
+    public boolean success;
+    public String message;
+
+}
